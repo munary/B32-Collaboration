89c121053241f3ff3abf7dd4adac73bb86a76664
Frederic <fred.theriault@gmail.com>
2017-04-24 16:35:07 -0400

    Added base file

---
+++ main.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+int count = 0;
+
+/*
+ * Fonction du prof!
+*/
+void fct00() {
+    cout << "     ,.----------.     \n";
+    cout << "    ((|          |     \n";
+    cout << "   .--\\          /--.  \n";
+    cout << "  '._  '========'  _.' \n";
+    cout << "     `""""""""""""`    \n";
+    cout << "Le cafe est le meilleur ami de l'informaticien !\n";
+}
+
+void fct01() {
+}
+
+void fct02() {
+}
+
+void fct03() {
+}
+
+void fct04() {
+}
+
+void fct05() {
+}
+
+void fct06() {
+}
+
+
+void fct07() {
+}
+
+void fct08() {
+}
+
+void fct09() {
+}
+
+void fct10() {
+}
+
+void fct11() {
+}
+
+void fct12() {
+}
+
+void fct13() {
+}
+
+void fct14() {
+}
+
+void fct15() {
+}
+
+void fct16() {
+}
+
+void fct17() {
+}
+
+void fct18() {
+}
+
+void fct19() {
+}
+
+void fct20() {
+}
+
+void fct21() {
+}
+
+void fct22 () {
+}
+
+void fct23() {
+}
+
+void fct24() {
+}
+
+void fct25() {
+}
+
+void fct26() {
+}
+
+void fct27() { 
+}
+
+void fct28() {
+}
+
+void fct29() {
+}
+
+void fct30() {
+}
+
+void separator() {	
+	cout << "\n======= fct " << count++ << " ===============\n";
+}
+
+int main(int argc, char* argv[])
+{
+	separator();
+	fct00();
+	separator();
+	fct01();
+	separator();
+	fct02();
+	separator();
+	fct03();
+	separator();
+	fct04();
+	separator();
+	fct05();
+	separator();
+	fct06();
+	separator();
+	fct07();
+	separator();
+	fct08();
+	separator();
+	fct09();
+	separator();
+	fct10();
+	separator();
+	fct11();
+	separator();
+	fct12();
+	separator();
+	fct13();
+	separator();
+	fct14();
+	separator();
+	fct15();
+	separator();
+	fct16();
+	separator();
+	fct17();
+	separator();
+	fct18();
+	separator();
+	fct19();
+	separator();
+	fct20();
+	separator();
+	fct21();
+	separator();
+	fct22();
+	separator();
+	fct23();
+	separator();
+	fct24();
+	separator();
+	fct25();
+	separator();
+	fct26();
+	separator();
+	fct27();
+	separator();
+	fct28();
+	separator();
+	fct29();
+	separator();
+	fct30();
+	
+	return 0;
+}
